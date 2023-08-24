9aa121cee9c19cdf2bd755ca409930d9d14ba601
existenz31 <spaltrie1@gmail.com>
2023-08-24 14:11:03 +0200

    cleaning

--- index.js
+++ index.js
@@ -1,34 +1,30 @@
 
 'use strict';
+
 const algoliasearch = require('algoliasearch');
-const algoliasearchHelper = require('algoliasearch-helper');
 
 const fs = require('fs');
 const csv=require('csvtojson')
-
+var _ = require('lodash');
 require('dotenv').config(); 
 
 
 const APPLICATION_ID = process.env.APPLICATION_ID;
 const API_KEY_SETTINGS = process.env.API_KEY_SETTINGS;
-const API_KEY_SEARCH = process.env.API_KEY_SEARCH;
 const RESTAURANTS_INDEX = process.env.RESTAURANTS_INDEX;
 const API_KEY_LOAD_DATA = process.env.API_KEY_LOAD_DATA;
 
 const RESTAURANTS_JSON_FILE = process.env.RESTAURANTS_JSON_FILE;
 const RESTAURANTS_CSV_FILE = process.env.RESTAURANTS_CSV_FILE;
 
-var searchHelper = null;
-
 async function initIndexSettings() {
     const client = algoliasearch(APPLICATION_ID, API_KEY_SETTINGS);
     const index = client.initIndex(RESTAURANTS_INDEX);
     
-    return index.setSettings({
-        'searchableAttributes': ['name', 'area', '_geoloc'],
-        'attributesForFaceting': ['searchable(food_type)', 'payment_options']
+    return await index.setSettings({
+        'searchableAttributes': ['name', 'area', 'neighborhood', '_geoloc'],
+        'attributesForFaceting': ['searchable(food_types)', 'payment_options']
       }).then(() => {
-        console.log('all good');
         return true;
       });    
       // manually added the One-way Synonym Discover => Diners Club, Carte Blanche. Did not work :(
@@ -39,40 +35,27 @@ async function loadJson() {
     const index = client.initIndex(RESTAURANTS_INDEX);
     let jsonData = fs.readFileSync(RESTAURANTS_JSON_FILE);
     let restaurants = JSON.parse(jsonData);
-    console.log('Nb Restaurants Loaded', restaurants.length);
-    console.log('res', restaurants[0]);
     // Cleanup the data before loading
     for (let r of restaurants) {
-        // delete r.price; // useless
+        // remove useless fields
+        delete r.address; 
+        delete r.postal_code; 
+        delete r.dining_style; 
         // clean the payment options => 
-        // --- remove Cash Only / JBC / Pay with OpenTable
-        // --- replace Diners Club / Carte Blanche => Discover
-        let options = r.payment_options;
-        let curated = [];
-        let addDiscover = false
-        for (let o of options) {
-            if (o === 'Cash Only' || o === 'JCB' || o === 'Pay with OpenTable') {
-                continue;
-            } 
-            if (o === 'Diners Club' || o === 'Carte Blanche' || o === 'Discover') {
-                addDiscover = true;
-            }
-            else {
-                curated.push(o);
-            }
-        }
-        if (addDiscover) {
+        r.payment_options = _.without(r.payment_options, 'Cash Only', 'JCB', 'Pay with OpenTable');
+        let curated = _.without(r.payment_options, 'Diners Club', 'Carte Blanche', 'Discover');
+        if (curated.length < r.payment_options.length) {
             curated.push('Discover');
         }
         r.payment_options = curated;
-
     } 
-    index.saveObjects(restaurants, {
+
+    await index.saveObjects(restaurants, {
         autoGenerateObjectIDIfNotExist: false
         // Any other requestOptions
-      }).then(({ objectIDs }) => {
-        console.log(objectIDs);
-      });
+      }).then((res) => {
+        console.log('Nb Saved: ', res.objectIDs.length);
+    });
 
 }
 
@@ -83,79 +66,209 @@ async function loadCSV() {
     let restaurants = await csv({
         delimiter: ';'
     }).fromFile(RESTAURANTS_CSV_FILE);
-    console.log('Nb Restaurants Loaded', restaurants.length);
-    console.log('res', restaurants[0]);
+
     // Cleanup the data before loading
     for (let r of restaurants) {
         r.stars_count = parseFloat(r.stars_count);
-        //r.stars_count_facet = Math.floor(r.stars_count);
         r.reviews_count = parseInt(r.reviews_count);
         r.phone = r.phone_number; // use this as the phone number
         delete r.phone_number;
+        // Clean a bit the food_type as an array of types
+        switch (r.food_type) {
+            case 'Contemporary American':
+                r.food_types = ['American'];
+                //r._tags = ['Contemporary'];
+                break;
+            case 'Contemporary French':
+                r.food_types = ['French'];
+                //r._tags = ['Contemporary'];
+                break;
+            case 'Contemporary Italian':
+                r.food_types = ['Italian'];
+                //r._tags = ['Contemporary'];
+                break;    
+            case 'Sicilian':
+                r.food_types = ['Italian'];
+                //r._tags = ['Sicilian'];
+                break;    
+    
+            case 'Contemporary European':
+                r.food_types = ['European'];
+                //r._tags = ['Contemporary'];
+                break;    
+            case 'Modern European':
+                r.food_types = ['European'];
+                //r._tags = ['Modern'];
+                break;        
+            case 'Contemporary French / American':
+                r.food_types = ['French', 'American'];
+                //r._tags = ['Contemporary'];
+                break;    
+            case 'Contemporary Asian':
+                r.food_types = ['Asian'];
+                //r._tags = ['Contemporary'];
+                break;    
+            case 'Pan-Asian':
+                r.food_types = ['Asian'];
+                //r._tags = ['Pan'];
+                break;    
+            case 'Contemporary Southern':
+                r.food_types = ['American'];
+                //r._tags = ['Contemporary', 'Southern'];
+                break;            
+            case 'Contemporary Mexican':
+                r.food_types = ['Mexican'];
+                //r._tags = ['Contemporary'];
+                break;                    
+            case 'Traditional Mexican':
+                r.food_types = ['Mexican'];
+                //r._tags = ['Traditional'];
+                break;                    
+            case 'Regional Mexican':
+                r.food_types = ['Mexican'];
+                //r._tags = ['Regional'];
+                break;                    
+            case 'Tex-Mex Mexican':
+                r.food_types = ['Mexican'];
+                //r._tags = ['Tex-Mex'];
+                break;                            
+            case 'Mexican / Southwestern':
+                r.food_types = ['Mexican', 'American'];
+                break;        
+            case 'Contemporary Indian':
+                r.food_types = ['Indian'];
+                //r._tags = ['Contemporary'];
+                break;            
+            case 'French American':
+                r.food_types = ['French', 'American'];
+                break;            
+            case 'Brazilian Steakhouse':
+                r.food_types = ['Brazilian'];
+                //r._tags = ['Steakhouse'];
+                break;            
+            case 'Steakhouse':
+                r.food_types = ['American'];
+                break;
+            case 'Steakhouse':
+                r.food_types = ['American'];
+                //r._tags = ['Steakhouse'];
+                break;
+            case 'Steak':
+                r.food_types = ['American'];
+                //r._tags = ['Steak'];
+                break;
+            case 'Barbecue':
+                r.food_types = ['American'];
+                //r._tags = ['Barbecue'];
+                break;
+            case 'Burgers':
+                r.food_types = ['American'];
+                //r._tags = ['Burgers'];
+                break;
+            case 'Prime Rib':
+                r.food_types = ['American'];
+                //r._tags = ['Prime Rib'];
+                break;
+    
+            case 'Northwest':
+                r.food_types = ['American'];
+                //r._tags = ['Northwest'];
+                break;
+            case 'Southern':
+                r.food_types = ['American'];
+                //r._tags = ['Southern'];
+                break;
+            case 'Southwest':
+                r.food_types = ['American'];
+                //r._tags = ['Southwest'];
+                break;    
+            case 'Contemporary Southern':
+                r.food_types = ['American'];
+                //r._tags = ['Contemporary', 'Southern'];
+                break;        
+            case 'South American':
+                r.food_types = ['American'];
+                //r._tags = ['South']
+                break;            
+            case 'South Indian':
+                r.food_types = ['Indian'];
+                //r._tags = ['South'];
+                break;                
+            case 'Southeast Asian':
+                r.food_types = ['Asian'];
+                //r._tags = ['Southeast'];
+                break;                
+            case 'Gastro Pub':
+                r.food_types = ['French'];
+                //r._tags = ['Gastro Pub'];
+                break;
+            case 'Tapas / Small Plates':
+                r.food_types = ['Spanish'];
+                //r._tags = ['Tapas / Small Plates'];
+                break;
+            case 'Sushi':
+                r.food_types = ['Japanese'];
+                //r._tags = ['Sushi'];
+                break;         
+            case 'Global, International':
+                r.food_types = ['International'];
+                //r._tags = ['Global'];
+                break;
+            case 'Latin / Spanish':
+                r.food_types = ['Latin American', 'Spanish'];
+                break;
+            case 'Basque':
+                r.food_types = ['French'];
+                //r._tags = ['Basque'];
+                break;
+            case 'Provencal':
+                r.food_types = ['French'];
+                //r._tags = ['Provencal'];
+                break;
+            case 'Fondue':
+                r.food_types = ['French'];
+                //r._tags = ['Fondue'];
+                break;
+        }
+        if (!r.food_types) {
+           r.food_types = [r.food_type];
+        }
     } 
-    index.partialUpdateObjects(restaurants, {
+    await index.partialUpdateObjects(restaurants, {
         createIfNotExists: false
-      }).then(({ objectIDs }) => {
-        console.log(objectIDs);
-      });
-
-}
-
-
-function renderHits(content) {
-    console.log('renderHits');
-//    $('#container').html(JSON.stringify(content, null, 2));
-}
-
-function initHelper() {
-    const client = algoliasearch(APPLICATION_ID, API_KEY_SEARCH);
-    searchHelper = algoliasearchHelper(client, RESTAURANTS_INDEX, {
-        disjunctiveFacets: ['payment_options', 'food_type'],
-        hitsPerPage: 3,
-        maxValuesPerFacet: 7
-    });
-
-    searchHelper.on('result', function (event) {
-        renderHits(event.results);
+      }).then((res) => {
+        console.log('Nb partial Updates: ', res.objectIDs.length);
       });
 }
 
-  async function searchRestaurants(search, food_types, stars_min, payment_options) {
-      
-    searchHelper.clearRefinements();
-    for (const ft of food_types) {
-        searchHelper.addDisjunctiveFacetRefinement('food_type', ft);
-    }
-    for (const po of payment_options) {
-        searchHelper.addDisjunctiveFacetRefinement('payment_options', po);
-    }
-    if (search) {
-        searchHelper.setQuery(search);
-    }
-    if (stars_min) {
-        searchHelper.addNumericRefinement ('stars_count', '>=', stars_min);
-    }
-    searchHelper.search();
 
 
-    // let numericFilters = [];
-    // let filters 
-    // if (stars_min) numericFilters.push(`stars_count > ${stars_min}`);
-    // index.search(search, {
-    //     numericFilters,
-    //     hitsPerPage: 3
-    // }).then((result) => {
-    //     console.log(result.hits);
-    //   });
+if (process.argv.length === 2) {
+    console.error('Expected at least one argument: init, json, csv');
+    process.exit(1);
+  }
+const arg = process.argv[2];
+
+switch (arg) {
+    case 'init':
+        console.log('Start Initializing the Index');
+        initIndexSettings();
+        console.log('Index has Initialized');
+        break;
+    case 'json':
+        console.log('Start loading Json file');
+        loadJson();
+        console.log('Json file has loaded');
+        break;
+    case 'csv':
+        console.log('Start loading CSV file');
+        loadCSV();
+        console.log('CSV file has loaded');
+        break;
+    default:
+        console.error('Expected argument: init, json, csv');
+        process.exit(1);       
 
 }
-const search = 'Grill';
-const food_types = ['American', 'French', 'Italian'];
-const stars_min = 4;
-const payment_options = ['AMEX'];
 
-//initIndexSettings();
-//loadJson();
-//loadCSV();
-initHelper();
-searchRestaurants(search, food_types, stars_min, payment_options);
+
---
+++ ui.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const algoliasearch = require('algoliasearch');
+
+const algoliasearchHelper = require('algoliasearch-helper');
+var _ = require('lodash');
+require('dotenv').config(); 
+
+const API_KEY_SEARCH = process.env.API_KEY_SEARCH;
+
+var searchHelper = null;
+function renderHits(content) {
+    console.log('renderHits');
+//    $('#container').html(JSON.stringify(content, null, 2));
+}
+
+function initHelper() {
+    const client = algoliasearch(APPLICATION_ID, API_KEY_SEARCH);
+    searchHelper = algoliasearchHelper(client, RESTAURANTS_INDEX, {
+        disjunctiveFacets: ['payment_options', 'food_type'],
+        hitsPerPage: 3,
+        maxValuesPerFacet: 7
+    });
+
+    searchHelper.on('result', function (event) {
+        renderHits(event.results);
+      });
+}
+
+  async function searchRestaurants(search, food_types, stars_min, payment_options) {
+      
+    searchHelper.clearRefinements();
+    for (const ft of food_types) {
+        searchHelper.addDisjunctiveFacetRefinement('food_type', ft);
+    }
+    for (const po of payment_options) {
+        searchHelper.addDisjunctiveFacetRefinement('payment_options', po);
+    }
+    if (search) {
+        searchHelper.setQuery(search);
+    }
+    if (stars_min) {
+        searchHelper.addNumericRefinement ('stars_count', '>=', stars_min);
+    }
+    searchHelper.search();
+
+
+    // let numericFilters = [];
+    // let filters 
+    // if (stars_min) numericFilters.push(`stars_count > ${stars_min}`);
+    // index.search(search, {
+    //     numericFilters,
+    //     hitsPerPage: 3
+    // }).then((result) => {
+    //     console.log(result.hits);
+    //   });
+
+}
+const search = 'Grill';
+const food_types = ['American', 'French', 'Italian'];
+const stars_min = 4;
+const payment_options = ['AMEX'];
+
+// initHelper();
+// searchRestaurants(search, food_types, stars_min, payment_options);
